e49747742451aca2eb0a12524224e656c3644136
Gaba31 <brijeshgaba99@gmail.com>
2026-01-04 16:49:23 +0530

    Added security features using getters and setters

--- vehicle.py
+++ vehicle.py
@@ -1,8 +1,74 @@
 class Vehicle:
 
     def __init__(self, vehicle_id, model, battery_percentage):
-        self.vehicle_id = vehicle_id
-        self.model = model
-        self.battery_percentage = battery_percentage
+        self.vehicle_id = int(vehicle_id)
+        self.model = str(model)
+        # self.battery_percentage = None
+        self.set_battery_percentage(battery_percentage)
+        self.__maintenance_status = None
+        self.__rental_price = None
+
+    def set_rental_price(self,price):
+        """
+            Sets the rental price of the vehicle
+
+            Parameter
+            ---------
+            price : int
+                Rental price value
+
+            Raises
+                ValueError
+                    If the rental price is less than 0
+        """
+        if isinstance(price,(int,float)) and price>=0:
+            self.__rental_price = int(price)
+        else:
+            raise ValueError("Rental price cannot be negative")
+
+    def get_rental_price(self):
+        """
+            This will return rental status of the vehicle.
+        """
+        return self.__rental_price
+
+    def get_maintenance_status(self):
+        """
+            This will return maintainace status of the vehicle.
+        """
+        return self.__maintenance_status
+
+    def set_maintenance_status(self, status):
+        """
+             Sets the maintance status of the vehicle.
+
+             Parameters
+             ----------
+                status :
+
+             Raises
+             ----------
+
+        """
+        self.__maintenance_status = str(status)
+
+    def set_battery_percentage(self,battery_percentage):
+        """
+              Sets the battery percentage of the vehicle.
+
+              Parameters
+              ----------
+              battery_percentage : int
+                  Battery percentage value
+
+              Raises
+              ------
+              ValueError
+                  If battery percentage is out of range
+              """
+        if isinstance (battery_percentage,(int,float)) and(battery_percentage >= 0 and battery_percentage<=100):
+            self.battery_percentage = int(battery_percentage)
+        else:
+            raise ValueError("Battery Percentage must be between 0 and 100")
 
 
